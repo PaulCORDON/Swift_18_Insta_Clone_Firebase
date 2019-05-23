79586f1b37906c61505b14b9c7c6ae12f3c68c58
Paul <paul.cordon@laposte.net>
2019-05-23 12:27:41 +0200

    Initial Commit

---
+++ Insta Clone Firebase/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Insta Clone Firebase
+//
+//  Created by MAC-DIN-002 on 23/05/2019.
+//  Copyright © 2019 MAC-DIN-002. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
